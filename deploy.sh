487cf3a84666e16bf80b00eb06386892185a6e97
LiaoLanFei <1252650992@qq.com>
2023-01-09 22:00:46 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-# npm run docs:build
+npm run docs:build
 
-# cd docs/.vitepress/dist
+cd docs/.vitepress/dist
 
 git init 
 git add -A
-git commit -m 'deploy测试'
-# git push -f https://github.com/LLF14/docs.git master:master
-git push https://github.com/LLF14/docs.git master:master
+git commit -m '修复搜索报错'
+git push -f https://github.com/LLF14/docs.git master:gh-pages
+
 cd -
-# rm -rf docs/.vitepress/dist
+rm -rf docs/.vitepress/dist
