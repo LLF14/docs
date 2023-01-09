6cb9a00e8b768acae155cb5e5a03cfefcb1ca166
LiaoLanFei <1252650992@qq.com>
2023-01-09 21:32:11 +0800

    deploy测试

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy测试'
 # git push -f https://github.com/LLF14/docs.git master:master
-git push  https://github.com/LLF14/docs.git master:master
+git push  https://github.com/LLF14/docs.git master
 cd -
 # rm -rf docs/.vitepress/dist
